0d565e3b2ec24484b8dc86a7bf0c0c9a6dbf03a8
Edipo Elwes <edipoelwes2@gmail.com>
2019-08-12 21:40:31 -0300

    configurando o banco de dados mysql

--- ToDo/ToDo/settings.py
+++ ToDo/ToDo/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import pymysql
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -73,10 +75,17 @@ WSGI_APPLICATION = 'ToDo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+pymysql.install_as_MySQLdb()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'ToDo',
+        'USER': 'root',
+        'PASSWORD': '',
+        'PORT': '3307',
     }
 }
 
